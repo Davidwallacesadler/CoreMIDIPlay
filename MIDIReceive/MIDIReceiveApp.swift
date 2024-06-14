96714fe5c55b2c64263e19f489cab348ae41619f
David Sadler <davidwallacesadler@gmail.com>
2024-06-13 21:04:08 -0600

    Initial Commit

---
+++ MIDIReceive/MIDIReceiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDIReceiveApp.swift
+//  MIDIReceive
+//
+//  Created by David Sadler on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MIDIReceiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
